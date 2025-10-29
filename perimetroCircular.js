6eb785861c1974c7f84b3d1287850b433c873fb8
Carlos Matielli <93890561+carlos-asantos@users.noreply.github.com>
2025-10-29 11:03:47 -0300

    Add perimeter calculation for circular room
    Implement basic calculation for circular room perimeter

---
+++ perimetroCircular.js
@@ -0,0 +1,6 @@
+// Código básico para calcular o perímetro de uma sala circular
+
+const pi = 3.14
+let raio = prompt("Digite o raio da sala:");
+const perimetro = (2 * pi * raio).toFixed(2);
+console.log(perimetro)
