af6aa0559054a2a9b57b8fd9ffec79cc29fded1b
Carlos Alexandre <carlosmatiellisantos@gmail.com>
2025-11-06 18:00:40 -0300

    Refactor task creation functionality and enhance styling for task elements

--- checklist w js/script.js
+++ checklist w js/script.js
@@ -4,7 +4,21 @@ function pegarNomeDaTask() {
     limparCampo()
 }
 
+function createTask() {
+    let criarTasks = document.getElementById('tasksList');
+    const taskName = pegarNomeDaTask();
+    let texto = `<div class="task"><p>${taskName}</p><input type="checkbox" id="taskFeita"></div>`;
+    criarTasks.innerHTML = texto;
+}
+
 function limparCampo() {
     campo = document.querySelector('textarea');
     campo.value = '';
+}
+
+function taskFoiFeita() {
+    const taskFeita = document.getElementById('taskFeita').checked;
+    if(taskFeita) {
+        
+    }
 }
