e7533173863327e9f1c5433269de804af9d93aa8
Carlos Alexandre <carlosmatiellisantos@gmail.com>
2025-11-10 16:51:06 -0300

    Update task management interface with improved task checkbox functionality and styling

--- checklist w js/script.js
+++ checklist w js/script.js
@@ -1,14 +1,16 @@
 function pegarNomeDaTask() {
     let nomeDaTask = document.querySelector('textarea').value;
-    console.log(nomeDaTask);
     limparCampo()
+    return nomeDaTask;
 }
 
 function createTask() {
+    let arrayDasTasks = document.getElementsByClassName('task');
     let criarTasks = document.getElementById('tasksList');
     const taskName = pegarNomeDaTask();
-    let texto = `<div class="task"><p>${taskName}</p><input type="checkbox" id="taskFeita"></div>`;
-    criarTasks.innerHTML = texto;
+    let numeroDaTask = arrayDasTasks.length
+    const texto = `<div class="task"><p>${taskName}</p><input type="checkbox" class="checkbox " id="taskFeita" onchange="taskFoiFeita(${numeroDaTask})"></div>`;
+    criarTasks.innerHTML += texto;
 }
 
 function limparCampo() {
@@ -16,9 +18,7 @@ function limparCampo() {
     campo.value = '';
 }
 
-function taskFoiFeita() {
-    const taskFeita = document.getElementById('taskFeita').checked;
-    if(taskFeita) {
-        
-    }
+function taskFoiFeita(numeroDaTask) {
+    let tasks = document.getElementsByClassName('task');
+    tasks[numeroDaTask].classList.add('taskFoiFeita');
 }
